d22442e80c0eb89c084e99cb55dc569f94b37d2a
Dusty Jewett <dusty.jewett@gmail.com>
2015-03-08 14:36:55 -0700

    Adding an endpoint that mocks a JSON API endpoint.

---
+++ server/mocks/todos.js
@@ -0,0 +1,42 @@
+module.exports = function(app) {
+  var express = require('express');
+  var todosRouter = express.Router();
+
+  todosRouter.get('/', function(req, res) {
+    res.send({
+      'todos': [{
+        id:1,
+
+        createdAt:new Date(2015, 1, 1),
+        updatedAt:new Date(2015, 1, 2),
+        dueDate:new Date(2016, 5, 5)
+      }]
+    });
+  });
+
+  todosRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  todosRouter.get('/:id', function(req, res) {
+    res.send({
+      'todos': {
+        id: req.params.id
+      }
+    });
+  });
+
+  todosRouter.put('/:id', function(req, res) {
+    res.send({
+      'todos': {
+        id: req.params.id
+      }
+    });
+  });
+
+  todosRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/1/todos', todosRouter);
+};
