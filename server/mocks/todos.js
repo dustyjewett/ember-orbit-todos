5478dc5d8daf102e7e31f23a1d4a5543e9d71b67
Dusty Jewett <dusty.jewett@gmail.com>
2015-03-14 13:05:27 -0700

    Adding a semi-persistant array to help simulate server calls.

--- server/mocks/todos.js
+++ server/mocks/todos.js
@@ -1,28 +1,42 @@
+var _ = require('lodash');
 module.exports = function(app) {
+
+  // This is an important bit. Orbit's JSONSource uses a different mime type,
+  // which may not be parsed by your backend-of-choice
+  app.use(require('body-parser').json({
+    type:'application/vnd.api+json'
+  }));
+
   var express = require('express');
   var todosRouter = express.Router();
 
+  var todoMockDatabase = {
+    "1":{
+      id:"1",
+      title:'Add to Orbit.JS Documentation',
+      createdAt:new Date(2015, 1, 1),
+      updatedAt:new Date(2015, 1, 2),
+      deletedAt:undefined,
+      dueDate:new Date(2016, 5, 5)
+  }};
+
   todosRouter.get('/', function(req, res) {
     res.send({
-      'todos': [{
-        id:1,
-        title:'Add to Orbit.JS Documentation',
-        createdAt:new Date(2015, 1, 1),
-        updatedAt:new Date(2015, 1, 2),
-        dueDate:new Date(2016, 5, 5)
-      }]
+      'todos': _.values(todoMockDatabase)
     });
   });
 
   todosRouter.post('/', function(req, res) {
-    res.status(201).end();
+    item = req.body.todos;
+    item.createdAt = new Date();
+    item.updatedAt = new Date();
+
+    res.status(201).send({"todos":item});
   });
 
   todosRouter.get('/:id', function(req, res) {
     res.send({
-      'todos': {
-        id: req.params.id
-      }
+      'todos': todoMockDatabase[req.params.id]
     });
   });
 
